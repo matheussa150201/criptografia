5c5e3feef800404f32a7ba7afcd2ff86af7c097f
matheussa150201 <matheussadejesus123@gmail.com>
2022-08-21 22:06:19 -0300

    FEAT - Adicionando criptografia a senha

---
+++ src/test/java/com/matheus/sa/criptografia/CriptografiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheus.sa.criptografia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CriptografiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
